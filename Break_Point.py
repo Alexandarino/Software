33323f681d3a6f25a8b92f04f0d1ffeeb236827d
Oussama Malass <malassouss@gmail.com>
2024-04-20 10:30:53 +0300

    Update README.md

---
+++ Break_Point.py
@@ -0,0 +1,33 @@
+def add(a, b):
+    result = a + b
+    return result
+
+def subtract(a, b):
+    result = a - b
+    return result
+
+def multiply(a, b):
+    result = a * b
+    return result
+
+def divide(a, b):
+    result = a / b
+    return result
+
+def main():
+    x = 10
+    y = 5
+    
+    
+    z = add(x, y)
+    
+    z = subtract(z, y)
+    
+    z = multiply(z, y)
+    
+    z = divide(z, 0)
+    
+    print("Result:", z)
+
+if __name__ == "__main__":
+    main()
