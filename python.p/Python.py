cc72f2e67379c93668657a566301919c30d1bb8c
Oussama Malass <malassouss@gmail.com>
2024-04-20 11:07:43 +0300

    new feature 3

--- python.p/Python.py
+++ python.p/Python.py
@@ -1,4 +1,5 @@
 print ("hello world")
 print ("this is a new feature")
 print ("this is a new feature 2")
-# git commit -n -m "new feature 2"
+print ("this is a new feature 3")
+# git commit -n -m "new feature 3"
